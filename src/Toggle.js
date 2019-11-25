49eb838dffa46c32bf344caf379dd0cde9afdc86
Dan Krishnan <dan@wearecodenation.com>
2019-11-25 21:16:21 +0000

    simple toggle

--- src/Toggle.js
+++ src/Toggle.js
@@ -4,6 +4,8 @@ import { useSpring, animated } from "react-spring";
 
 const Toggle = () => {
   const [toggle, setToggle] = useState(false);
+  // no from property, just a to property which uses state to determine its value.
+  //dont have to specifically set it's from property, it will just use it's current value.
   const fade = useSpring({
     opacity: toggle ? 1 : 0
   });
